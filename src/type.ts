3f80c419bdcf06dfc23c1a3f997b731ec188c173
aShirBaskenazi <shirbas123@gmail.com>
2023-01-11 21:06:49 +0200

    ex1_ex2_ex3

--- src/type.ts
+++ src/type.ts
@@ -6,3 +6,17 @@ function getAverage (arr:number[]): number{
     return sum/arr.length;
 }
 console.log(getAverage([23,43,53]));
+
+function getAmountOfPositive(arr:number[]):number{
+    let countOfP:number=0;
+    arr.forEach((value)=>{
+        if (value>0){
+            countOfP++;
+        }
+    })
+    return countOfP;
+}
+
+function sortList(arr:number[]){
+    return arr.sort();
+}
